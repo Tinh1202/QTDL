3bc37a625697c67d3b9c8c58a797ae9387894fea
Tinh Vo <vntinhwork2003@gmail.com>
2024-10-21 16:47:45 +0700

    create account login

---
+++ src/Model/User_Account.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author vntin
+ */
+public class User_Account { // account login 
+    private String username;
+    private String password;
+    
+    /*
+        create constructor
+    */
+    User_Account(){
+        
+    }
+    
+    // create getter, setter
+    /*
+        code *
+    */
+    
+    
+    // SHA-256/bcrypt
+    public User_Account hashingAccount(String username, String password){ // hashing password and username
+        // input -> hashing
+        return null;
+    }
+    
+    
+    public boolean CheckAccount(User_Account account){
+        // connected -> get Account from database(also hash) -> hash (hashingAccount) -> check
+        return false;
+    }
+    
+    
+    public void Login(String username, String password){ // function check password and username
+        /* if true do ... 
+           if false do ... 
+        */ 
+    } 
+    
+   
+}
