a093201f577a6b10696c4730647995ef75b2aa8c
Tinh Vo <vntinhwork2003@gmail.com>
2024-10-21 16:46:59 +0700

    create Staff class

---
+++ src/Model/Staff.java
@@ -0,0 +1,46 @@
+package Model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vntin
+ */
+import Model.Person;
+import java.util.ArrayList;
+
+public class Staff extends Person {
+    private String position;
+    
+    Staff(){
+        super();
+        this.position = new String("");
+    }
+    
+    // create Staff(..., position) {}
+    
+    
+    
+    /*
+    create getter, setter position
+    */
+    
+    
+    Staff(Staff s){  // constructor object
+       // code *
+    }
+    
+    
+    public String toString(){
+        return null; // return ..., position
+    }
+    
+    private ArrayList ListStaff () {
+        return null; // return list of staff
+    }
+    
+    
+}
